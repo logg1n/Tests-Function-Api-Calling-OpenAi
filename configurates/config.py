30ac59263a2f8aa7d0b99ad014a5dd5b425f15fa
Stanislav <46058751+logg1n@users.noreply.github.com>
2025-12-08 23:53:58 +0300

    Update config.py

--- configurates/config.py
+++ configurates/config.py
@@ -12,11 +12,3 @@ USE_CACHE = os.getenv("USE_CACHE", "false").lower() == "true"
 if not USE_CACHE:
     raise ValueError("API_TOKEN_TRELLO не установлен")
 
-API_KEY_TRELLO = os.getenv("API_KEY_TRELLO")
-if not API_KEY_TRELLO:
-    raise ValueError("API_KEY_TRELLO не установлен")
-
-
-API_TOKEN_TRELLO = os.getenv("API_TOKEN_TRELLO")
-if not API_TOKEN_TRELLO:
-    raise ValueError("API_TOKEN_TRELLO не установлен")
