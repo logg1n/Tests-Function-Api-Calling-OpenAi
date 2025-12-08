ececde0c2639edcf02c9e87535fe95976829209b
logg1n <loggiin13@gmail.com>
2025-12-08 23:07:10 +0300

    Извлекает из .env переменные окружения

--- configurates/config.py
+++ configurates/config.py
@@ -8,9 +8,14 @@ OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 if not OPENROUTER_API_KEY:
     raise ValueError("OPENROUTER_API_KEY не установлен")
 
+USE_CACHE = os.getenv("USE_CACHE", "false").lower() == "true"
+if not USE_CACHE:
+    raise ValueError("API_TOKEN_TRELLO не установлен")
+
 API_KEY_TRELLO = os.getenv("API_KEY_TRELLO")
 if not API_KEY_TRELLO:
     raise ValueError("API_KEY_TRELLO не установлен")
+
 
 API_TOKEN_TRELLO = os.getenv("API_TOKEN_TRELLO")
 if not API_TOKEN_TRELLO:
