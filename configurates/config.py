6d4199c138dd8a54ad76dd7079ec7f4d41f9603e
logg1n <loggiin13@gmail.com>
2026-01-03 21:01:33 +0300

    initial commit

--- configurates/config.py
+++ configurates/config.py
@@ -1,5 +1,5 @@
 import os
 
-OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY", "")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
 if not OPENROUTER_API_KEY:
     raise ValueError("OPENROUTER_API_KEY не установлен")
