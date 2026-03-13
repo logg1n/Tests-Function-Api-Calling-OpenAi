1baab54b2dced62d695f05a61ce4f7141f8b734e
logg1n <loggiin13@gmail.com>
2026-03-13 10:31:22 +0300

    new: script -> get all models and properties

---
+++ src/utils/get_all_model_openrouter.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get(
+    "https://api.proxyapi.ru/openrouter/v1/models",
+    headers={"Authorization": "Bearer <КЛЮЧ-PROXYAPI>"},
+)
+
+models = response.json()
+for model in models["data"]:
+    print(f"ID: {model['id']}")
+    print(f"Название: {model['name']}")
+    print(f"Стоимость ввода: ${model['pricing']['prompt']}")
+    print(f"Стоимость вывода: ${model['pricing']['completion']}")
+    print("---")
