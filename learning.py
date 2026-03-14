7b963407c5f210a7e20c2f7e1b61e5a61229c649
logg1n <loggiin13@gmail.com>
2026-03-15 00:48:02 +0300

    fix:

--- learning.py
+++ learning.py
@@ -1,44 +1,13 @@
 import argparse
-import inspect
 import json
 import sys
-from importlib import import_module
-from pathlib import Path
 
-import orjson
+import yaml
 
+from src.schema.client_schema import ClientModel
 from src.schema.json_schema import Schema
-from src.schema.py_schema import FunctionSchema
 
 sys.tracebacklimit = 0
-
-
-def parse_json(file) -> Schema:
-    with open(file, encoding="utf-8") as f:
-        raw_schema = orjson.loads(json.dumps(json.load(f)))
-
-    return Schema.model_validate(raw_schema)
-
-
-def parse_func(file: Path, schema: Schema) -> bool:
-    import_path = ".".join(file.with_suffix("").parts)
-
-    module = import_module(import_path)
-    func = getattr(module, file.stem)
-
-    sig = inspect.signature(func)
-
-    source = inspect.getsource(func)
-
-    data = {
-        "arguments": sig.parameters,
-        "json_schema": schema,
-        "source_code": source,
-    }
-
-    FunctionSchema.model_validate(data)
-
-    return True
 
 
 def on_traceback():
@@ -48,19 +17,36 @@ def on_traceback():
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--trace", action="store_true")
-    parser.add_argument("--py", type=str, required=True)
-    parser.add_argument("--json", type=str, required=True)
+    parser.add_argument("--func", type=str, required=True)
+    parser.add_argument("--schema", type=str, required=True)
+    parser.add_argument("--conf", type=str, required=True)
     args: argparse.Namespace = parser.parse_args()
 
     if args.trace:
         on_traceback()
 
-    schema = parse_json(args.json)
-    path_func = Path(args.py)
-    res = parse_func(path_func, schema)
+    with open(args.schema, encoding="utf-8") as f:
+        schema_dict = json.load(f)
+    schema = Schema.model_validate(schema_dict)
 
-    if not res:
-        raise Exception("Неизвестная ошибка")
+    with open(args.conf, encoding="utf-8") as f:
+        config_dict = yaml.safe_load(f)
+    conf = ClientModel.model_validate(**args.conf)
+
+    # module = import_module(import_path)
+    # func = getattr(module, file.stem)
+
+    # sig = inspect.signature(func)
+
+    # source = inspect.getsource(func)
+
+    # data = {
+    #     "arguments": sig.parameters,
+    #     "json_schema": schema,
+    #     "source_code": source,
+    # }
+
+    # FunctionSchema.model_validate(data)
 
 
 if __name__ == "__main__":
