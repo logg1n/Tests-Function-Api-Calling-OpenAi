61a9f9ee347cdfa44efd9017bf83c1779a9155b0
logg1n <loggiin13@gmail.com>
2026-03-14 20:19:28 +0300

    fix: -delete tetst type -> null

--- tests/tests_type_mismatch_json_to_python_json_schema.py
+++ tests/tests_type_mismatch_json_to_python_json_schema.py
@@ -38,7 +38,6 @@ def test_type_mismatch_error(file_name, schema_name, get_json_schema):
     [
         ("TypeMismatchJsonToPython.json", "test_sync_type_object"),
         ("TypeMismatchJsonToPython.json", "test_sync_type_array"),
-        # ("TypeMismatchJsonToPython.json", "test_sync_type_null"),
         ("TypeMismatchJsonToPython.json", "test_sync_type_number"),
         ("TypeMismatchJsonToPython.json", "test_sync_type_boolean"),
     ],
